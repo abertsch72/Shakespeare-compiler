337043ed7de693eb6cbb392bfa12499da3da9eb1
Amanda Bertsch <abertsch72@gmail.com>
2020-02-26 12:40:14 -0700

    grammar update, notes on scanner

--- src/tokenizing/Tokenize.java
+++ src/tokenizing/Tokenize.java
@@ -2,4 +2,38 @@ package tokenizing;
 
 public class Tokenize {
 
+	// tokenize one char then request more chars until non-alphabetic -- then check last thing for keywords
 }
+
+
+/*
+ * Tokens:
+ * 	ID
+ * 	END
+ * 	kwACT_OR_SCENE
+ * 	ROMAN_NUM
+ * 	COLON
+ * 	LBRACKET
+ * 	kwENTER
+ * 	kwEXIT
+ * 	kwEXUENT
+ * 	CHAR
+ * 	COMMA
+ * 	kwBYTE_OUTPUT
+ * 	kwCHAR_OUTPUT
+ * 	kwGOTO
+ * 	kwBYTE_INPUT
+ * 	kwCHAR_INPUT
+ * 	kwPUSH
+ * 	kwPOP
+ * 	kwME
+ * 	kwYOU
+ * 	POS_ADJ
+ * 	NEG_ADJ
+ * 	kwPOS
+ * 	kwNEG
+ * 	kwZERO
+ * 	kwIF_TRUE
+ * 	kwIF_FALSE
+ * 	RBRACKET
+ */
